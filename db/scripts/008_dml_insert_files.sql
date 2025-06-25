a3d59dac88f31c82773cdd31a999294298e1c505
kir <kirpro95@yandex.ru>
2025-06-25 20:26:40 +0300

    Заполнение БД и добавление постеров

---
+++ db/scripts/008_dml_insert_files.sql
@@ -0,0 +1,6 @@
+INSERT INTO files (name, path) VALUES
+('Книга Илая', 'files/book_of_elijah.webp'),
+('Хроники Риддика', 'files/chronicles_of_riddick.webp'),
+('Джон Уик', 'files/john_wick.webp'),
+('Титаник', 'files/titanic.webp'),
+('Звёздный путь', 'files/star_trek.webp');
---
+++ db/scripts/011_dml_insert_halls.sql
@@ -0,0 +1,4 @@
+INSERT INTO halls (name, row_count, place_count, description) VALUES
+('Зал 1', 10, 15, 'Большой зал с экраном IMAX'),
+('Зал 2', 8, 10, 'Обычный зал'),
+('Зал 3', 6, 8, 'Маленький уютный зал');
