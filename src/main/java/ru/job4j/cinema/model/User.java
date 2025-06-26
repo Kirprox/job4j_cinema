65f616f42c7bd5da93a7aed88fd4103eb1503542
kir <kirpro95@yandex.ru>
2025-06-26 12:31:56 +0300

    4. Создаем классы модели.

---
+++ src/main/java/ru/job4j/cinema/model/User.java
@@ -0,0 +1,16 @@
+package ru.job4j.cinema.model;
+
+import java.util.Map;
+
+public class User {
+    public static final Map<String, String> COLUMN_MAPPING = Map.of(
+            "id", "id",
+            "full_name", "fullName",
+            "email", "email",
+            "password", "password"
+    );
+    private int id;
+    private String fullName;
+    private String email;
+    private String password;
+}
